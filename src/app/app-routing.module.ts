54d7f17262a5c8372d054f99f66af05556b62395
Eduardo A. Cruz Junior <eduardo_acj2@hotmail.com>
2020-06-09 16:02:05 -0300

    reorganizacao do projeto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { UserLoginComponent } from './user/user-login/user-login.component';
-import { CadastroClienteComponent } from './cliente/cadastro-cliente/cadastro-cliente.component';
-import { DashboardComponent } from './admin/dashboard/dashboard.component';
-import { RelatorioClienteComponent } from './admin/cliente/relatorio-cliente/relatorio-cliente.component';
+import { UserLoginComponent } from './components/user/user-login/user-login.component';
+import { CadastroClienteComponent } from './components/cliente/cadastro-cliente/cadastro-cliente.component';
+import { DashboardComponent } from './components/admin/dashboard/dashboard.component';
+import { RelatorioClienteComponent } from './components/admin/cliente/relatorio-cliente/relatorio-cliente.component';
 
 const routes: Routes = [
   {path: '', component: CadastroClienteComponent},
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,18 @@ import { AngularFireModule } from '@angular/fire';
 import { AngularFireAuthModule } from '@angular/fire/auth';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { UserLoginComponent } from './user/user-login/user-login.component';
+import { UserLoginComponent } from './components/user/user-login/user-login.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AuthService } from './core/auth/auth.service';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { CadastroClienteComponent } from './cliente/cadastro-cliente/cadastro-cliente.component';
+import { CadastroClienteComponent } from './components/cliente/cadastro-cliente/cadastro-cliente.component';
 import { firebaseConfig } from 'src/environments/environment';
 import { NgxMaskModule } from 'ngx-mask';
-import { DashboardComponent } from './admin/dashboard/dashboard.component';
-import { MenuComponent } from './admin/menu/menu.component';
+import { DashboardComponent } from './components/admin/dashboard/dashboard.component';
+import { MenuComponent } from './components/admin/menu/menu.component';
 import { IconsModule } from './icons/feather/icons/icons.module';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { RelatorioClienteComponent } from './admin/cliente/relatorio-cliente/relatorio-cliente.component';
+import { RelatorioClienteComponent } from './components/admin/cliente/relatorio-cliente/relatorio-cliente.component';
 
 
 @NgModule({
