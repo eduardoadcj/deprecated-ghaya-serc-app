fd6bace72b7a35b5a064b249fa641284a66ca091
Eduardo A. Cruz Junior <eduardo_acj2@hotmail.com>
2020-06-24 22:12:08 -0300

    implementacao de modelagem

---
+++ src/app/model/cliente.ts
@@ -0,0 +1,13 @@
+import { Endereco } from './endereco';
+
+export class Cliente{
+    id: number;
+    nome: string;
+    email: string;
+    whatsapp: string;
+    numeroCalcado: string;
+    numeroJeans: string;
+    cpf: string;
+    nascimento: Date;
+    enderecos: Endereco[];
+}
---
+++ src/app/model/endereco.ts
@@ -0,0 +1,12 @@
+export class Endereco {
+    id: number;
+    titulo: string;
+    numero: string;
+    bairro: string;
+    cep: string;
+    cidade: string;
+    estado: string;
+    estadoUf: string;
+    complemento: string;
+    cliente_id: number;
+}
