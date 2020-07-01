bad3ead2070756c04e89027dbeaf6153542ee1f3
Eduardo A. Cruz Junior <eduardo_acj2@hotmail.com>
2020-07-01 15:02:22 -0300

    verificacao antes de estruturar a mascara

--- src/app/util/operators/mask-operator.ts
+++ src/app/util/operators/mask-operator.ts
@@ -1,6 +1,9 @@
 export class MaskOperator {
     
     formatCpf(cpf: string): string {
+        if(!cpf){
+            return '';
+        }
         return cpf.substring(0, 3) 
             + '.' + cpf.substring(3, 6) 
             + '.' + cpf.substring(6, 9) 
@@ -8,6 +11,9 @@ export class MaskOperator {
     }
 
     formatPhone(phone: string): string {
+        if(!phone){
+            return '';
+        }
         return '(' + phone.substring(0, 2) + ') ' 
             + phone.substring(2, 7) + '-' 
             + phone.substring(7);
